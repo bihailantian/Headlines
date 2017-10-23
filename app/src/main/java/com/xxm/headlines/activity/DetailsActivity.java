0632ac8f9cfa5dfc901dc84e7623f134b91063ea
Your Name <you@example.com>
2017-10-23 22:21:36 +0800

    1、修改列表的item

---
+++ app/src/main/java/com/xxm/headlines/activity/DetailsActivity.java
@@ -0,0 +1,18 @@
+package com.xxm.headlines.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.xxm.headlines.R;
+
+public class DetailsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+
+
+    }
+}
--- app/src/main/java/com/xxm/headlines/adapter/HomeItemAdapter.java
+++ app/src/main/java/com/xxm/headlines/adapter/HomeItemAdapter.java
@@ -30,14 +30,13 @@ public class HomeItemAdapter extends RecyclerView.Adapter<HomeItemAdapter.ViewHo
 
     @Override
     public int getItemCount() {
-        return mDatas != null ?mDatas.size():0;
+        return mDatas != null ? mDatas.size() : 0;
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view =  LayoutInflater.from(mContext).inflate( R.layout.rly_item_home, parent,false);
-//        View view = LayoutInflater.inflate(mContext, R.layout.rly_item_home, parent,false);
-        return  new ViewHolder(view);
+        View view = LayoutInflater.from(mContext).inflate(R.layout.rly_item_home, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
@@ -47,24 +46,24 @@ public class HomeItemAdapter extends RecyclerView.Adapter<HomeItemAdapter.ViewHo
         holder.tvAuthorName.setText(item.getAuthor_name());
         holder.tvDate.setText(item.getDate());
         Glide.with(mContext).load(item.getThumbnail_pic_s()).placeholder(R.mipmap.ic_launcher).into(holder.ivPic);
+        Glide.with(mContext).load(item.getThumbnail_pic_s02()).placeholder(R.mipmap.ic_launcher).into(holder.ivPic2);
+        Glide.with(mContext).load(item.getThumbnail_pic_s03()).placeholder(R.mipmap.ic_launcher).into(holder.ivPic3);
 
     }
 
 
+    class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView tvTitle, tvAuthorName, tvDate;
+        private ImageView ivPic,ivPic2,ivPic3;
 
-
-
-
-     class ViewHolder extends RecyclerView.ViewHolder {
-        private TextView tvTitle,tvAuthorName,tvDate;
-        private ImageView ivPic;
-
-         ViewHolder(View itemView) {
+        ViewHolder(View itemView) {
             super(itemView);
             tvTitle = (TextView) itemView.findViewById(R.id.tv_title);
             tvAuthorName = (TextView) itemView.findViewById(R.id.tv_author_name);
             tvDate = (TextView) itemView.findViewById(R.id.tv_date);
             ivPic = (ImageView) itemView.findViewById(R.id.iv_pic);
+            ivPic2 = (ImageView) itemView.findViewById(R.id.iv_pic2);
+            ivPic3 = (ImageView) itemView.findViewById(R.id.iv_pic3);
         }
     }
 }
